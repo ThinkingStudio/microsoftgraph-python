2bf30e94b57090ec0b340df88028ebe9cda9780b
Joe Cincotta <joe@grievelab.com>
2020-08-11 07:40:32 +1000

    setup to start working on this

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-setup(name='microsoftgraph-python',
+setup(name='ts-microsoftgraph-python',
       version='0.1.7',
       description='API wrapper for Microsoft Graph written in Python',
       long_description=read('README.md'),
-      url='https://github.com/GearPlug/microsoftgraph-python',
+      url='https://github.com/ThinkingStudio/microsoftgraph-python',
       long_description_content_type="text/markdown",
-      author='Miguel Ferrer, Nerio Rincon, Yordy Gelvez',
-      author_email='ingferrermiguel@gmail.com',
+      author='Miguel Ferrer, Nerio Rincon, Yordy Gelvez, James Martindale, Joe Cincotta',
+      author_email='joe@thinking.studio',
       license='MIT',
       packages=['microsoftgraph'],
       install_requires=[
