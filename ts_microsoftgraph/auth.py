108ed2a757b24254cb97e207eedbe0fdba92c8ac
Joey Pepperoni <joe@pixolut.com>
2020-08-17 22:42:01 +1000

    broken wip

--- ts_microsoftgraph/auth.py
+++ ts_microsoftgraph/auth.py
@@ -5,7 +5,7 @@ class Auth(object):
     def __init__(self, client_id: str, tenant_id: str, secret: str, scope=".default"):
         authority = "https://login.microsoftonline.com/" + tenant_id
         app = msal.ConfidentialClientApplication( client_id, authority=authority, client_credential=secret)
-        result = app.acquire_token_silent(scope, account=None)
+        result = app.acquire_token_silent(scopes=[scope], account=None)
         if not result:
             # No suitable token exists in cache. Let's get a new one from AAD
             result = app.acquire_token_for_client(scopes=[scope])
