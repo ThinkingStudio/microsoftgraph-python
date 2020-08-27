d5b783c1b95444d88fde8b75f9130bfef75984ba
Joey Pepperoni <joe@pixolut.com>
2020-08-27 12:20:49 +1000

    added context handling, fixed request formatting, and removed beta calls

--- ts_microsoftgraph/client.py
+++ ts_microsoftgraph/client.py
@@ -1,24 +1,17 @@
 import base64
 import mimetypes
 import requests
-import json
-from ts_microsoftgraph.auth import Auth
 from ts_microsoftgraph.decorators import token_required
 from ts_microsoftgraph.reponse_parser import parse
-from urllib.parse import urlencode, urlparse, quote_plus
 
-
-"""
-TODO: do I need to use another user context to read the mailbox? 
- 
-"""
 
 class Client(object):
     RESOURCE = 'https://graph.microsoft.com/'
-    def __init__(self, token, api_version='v1.0'):
-        self.api_version = api_version
-        self.base_url = self.RESOURCE + self.api_version + '/'
+    def __init__(self, token, api_version='v1.0', context='me'):
+        self._api_version = api_version
+        self._base_url = self.RESOURCE + self._api_version + '/'
         self.token = token
+        self._context = context
 
     @token_required
     def me(self, params=None):
@@ -35,7 +28,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + 'me', params=params)
+        return self._get(self._base_url + self._context, params=params)
 
     @token_required
     def subscription_create(self, change_type, notification_url, resource, expiration_datetime, client_state=None):
@@ -60,7 +53,7 @@ class Client(object):
             'expirationDateTime': expiration_datetime,
             'clientState': client_state
         }
-        return self._post(self.base_url + 'subscriptions', json=data)
+        return self._post(self._base_url + 'subscriptions', json=data)
 
     @token_required
     def subscription_renew(self, subscription_id, expiration_datetime):
@@ -79,7 +72,7 @@ class Client(object):
         data = {
             'expirationDateTime': expiration_datetime
         }
-        return self._patch(self.base_url + 'subscriptions/{}'.format(subscription_id), json=data)
+        return self._patch(self._base_url + 'subscriptions/{}'.format(subscription_id), json=data)
 
     @token_required
     def subscription_delete(self, subscription_id):
@@ -92,27 +85,9 @@ class Client(object):
             None.
 
         """
-        return self._delete(self.base_url + 'subscriptions/{}'.format(subscription_id))
+        return self._delete(self._base_url + 'subscriptions/{}'.format(subscription_id))
 
     # Mail
-    """
-    TODO: 
-    GET /me/mailFolders/inbox
-    Enumerate the list of well known folder names:
-    https://docs.microsoft.com/en-us/graph/api/resources/mailfolder?view=graph-rest-1.0
-
-    how to handle the object (id) to then pass into next requests? 
-
-    GET /me/mailFolders/{id}/messages
-    then, to select an individual message:
-    GET /me/mailFolders/{id}/messages/{id}
-
-    To get the MIME content of the specified message:
-    GET /me/messages/{id}/$value 
-
-
-    """
-
     @token_required
     def message_folder_list(self, params=None):
         """Retrieve the list of mailbox folders.
@@ -121,7 +96,7 @@ class Client(object):
         Returns:
             A dict.
         """
-        return self._get(self.base_url + '/me/mailFolders/', params=params)
+        return self._get(self._base_url + self._context + '/mailFolders/', params=params)
 
     @token_required
     def message_list(self, folder_id, params=None):
@@ -132,7 +107,7 @@ class Client(object):
         Returns:
             A dict.
         """
-        return self._get(self.base_url + '/me/mailFolders/{id}/messages'.format(id=folder_id), params=params)
+        return self._get(self._base_url + self._context + '/mailFolders/{id}/messages'.format(id=folder_id), params=params)
 
     @token_required
     def message_get(self, message_id, params=None, mime_content=False):
@@ -143,7 +118,7 @@ class Client(object):
         Returns:
             A dict.
         """
-        return self._get(self.base_url + 'me/messages/' + message_id + ("" if not mime_content else "/$value"), params=params)
+        return self._get(self._base_url + self._context + '/messages/' + message_id + ("" if not mime_content else "/$value"), params=params)
 
     @token_required
     def message_send(self, subject=None, recipients=None, body='', content_type='HTML', attachments=None):
@@ -186,7 +161,7 @@ class Client(object):
                      'SaveToSentItems': 'true'}
 
         # Do a POST to Graph's sendMail API and return the response.
-        return self._post(self.base_url + 'me/microsoft.graph.sendMail', json=email_msg)
+        return self._post(self._base_url + self._context + '/microsoft.graph.sendMail', json=email_msg)
 
     # Onenote
     @token_required
@@ -197,7 +172,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + 'me/onenote/notebooks')
+        return self._get(self._base_url + self._context + '/onenote/notebooks')
 
     @token_required
     def onenote_get(self, notebook_id):
@@ -210,7 +185,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + 'me/onenote/notebooks/' + notebook_id)
+        return self._get(self._base_url + self._context + '/onenote/notebooks/' + notebook_id)
 
     @token_required
     def onenote_sections(self, notebook_id):
@@ -223,7 +198,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + 'me/onenote/notebooks/{}/sections'.format(notebook_id))
+        return self._get(self._base_url + self._context + '/onenote/notebooks/{}/sections'.format(notebook_id))
 
     @token_required
     def onenote_create_page(self, section_id, files):
@@ -237,7 +212,7 @@ class Client(object):
             A dict.
 
         """
-        return self._post(self.base_url + '/me/onenote/sections/{}/pages'.format(section_id), files=files)
+        return self._post(self._base_url + self._context + '/onenote/sections/{}/pages'.format(section_id), files=files)
 
     @token_required
     def onenote_list_pages(self, params=None):
@@ -250,7 +225,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + '/me/onenote/pages', params=params)
+        return self._get(self._base_url + self._context + '/onenote/pages', params=params)
 
     # Calendar
     @token_required
@@ -264,7 +239,7 @@ class Client(object):
             A dict.
 
         """
-        return self._get(self.base_url + 'me/events')
+        return self._get(self._base_url + self._context + '/events')
 
     @token_required
     def calendar_create_event(self, subject, content, start_datetime, start_timezone, end_datetime, end_timezone,
@@ -315,197 +290,43 @@ class Client(object):
             },
             # "attendees": attendees_list
         }
-        url = 'me/calendars/{}/events'.format(calendar) if calendar is not None else 'me/events'
-        return self._post(self.base_url + url, json=body)
+        url = (self._context + '/calendars/{}/events'.format(calendar)) if calendar is not None else (self._context + '/events')
+        return self._post(self._base_url + url, json=body)
 
     @token_required
     def calendar_create(self, name):
-        """Create an event in the user's default calendar or specified calendar.
-
-        You can specify the time zone for each of the start and end times of the event as part of these values,
-        as the  start and end properties are of dateTimeTimeZone type.
-
-        When an event is sent, the server sends invitations to all the attendees.
-
-        Args:
-            name:
-
-        Returns:
-            A dict.
-
-        """
         body = {
             'name': '{}'.format(name)
         }
-        return self._post(self.base_url + 'me/calendars', json=body)
+        return self._post(self._base_url + self._context + '/calendars', json=body)
 
     @token_required
     def calendars_list(self):
-        """Get all the user's calendars (/calendars navigation property), get the calendars from the default
-        calendar group or from a specific calendar group.
-
-        Returns:
-            A dict.
-
-        """
-        return self._get(self.base_url + 'me/calendars')
+        return self._get(self._base_url + self._context + '/calendars')
 
     # Outlook
     @token_required
     def contacts_list(self, data_id=None, params=None):
-        if data_id is None:
-            url = "{0}me/contacts".format(self.base_url)
-        else:
-            url = "{0}me/contacts/{1}".format(self.base_url, data_id)
-        return self._get(url, params=params)
+        return self._get(self._base_url + self._context + "/contacts" + ("" if data_id is None else ("/" + data_id)), params=params)
 
     @token_required
     def contact_create(self, **kwargs):
-        url = "{0}me/contacts".format(self.base_url)
-        return self._post(url, **kwargs)
+        return self._post(self._base_url + self._context + "/contacts"  , **kwargs)
 
     @token_required
     def contact_create_in_folder(self, folder_id, **kwargs):
-        url = "{0}/me/contactFolders/{1}/contacts".format(self.base_url, folder_id)
-        return self._post(url, **kwargs)
+        return self._post(self._base_url + self._context + "/contactFolders/" + folder_id + "/contacts", **kwargs)
+
 
     @token_required
     def contact_folders(self, params=None):
-        url = "{0}me/contactFolders".format(self.base_url)
-        return self._get(url, params=params)
+        return self._get(self._base_url + self._context + "/contactFolders", params=params)
 
     @token_required
     def contact_create_folder(self, **kwargs):
-        url = "{0}me/contactFolders".format(self.base_url)
-        return self._post(url, **kwargs)
+        return self._post(self._base_url + self._context + "/contactFolders", **kwargs)
 
-    # Onedrive
-    @token_required
-    def drive_root_items(self, params=None):
-        return self._get('https://graph.microsoft.com/beta/me/drive/root', params=params)
-
-    @token_required
-    def drive_root_children_items(self, params=None):
-        return self._get('https://graph.microsoft.com/beta/me/drive/root/children', params=params)
-
-    @token_required
-    def drive_specific_folder(self, folder_id, params=None):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/children".format(folder_id)
-        return self._get(url, params=params)
-
-    @token_required
-    def drive_create_session(self, item_id, **kwargs):
-        url = "https://graph.microsoft.com/v1.0/me/drive/items/{0}/workbook/createSession".format(item_id)
-        # url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/createSession".format(item_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def drive_refresh_session(self, item_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/refreshSession".format(item_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def drive_close_session(self, item_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/closeSession".format(item_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def drive_download(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/content".format(item_id)
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def drive_get(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}".format(item_id)
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def drive_upload(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/content".format(item_id)
-        kwargs['headers'] = {'Content-Type': 'text/plain'}
-        return self._put(url, params=params, **kwargs)
-
-    # Excel
-    @token_required
-    def excel_get_worksheets(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets".format(item_id)
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def excel_get_names(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/names".format(item_id)
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def excel_add_worksheet(self, item_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/add".format(item_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def excel_get_specific_worksheet(self, item_id, worksheet_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}".format(item_id, quote_plus(worksheet_id))
-        return self._get(url, **kwargs)
-
-    @token_required
-    def excel_update_worksheet(self, item_id, worksheet_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}".format(item_id, quote_plus(worksheet_id))
-        return self._patch(url, **kwargs)
-
-    @token_required
-    def excel_get_charts(self, item_id, worksheet_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/charts".format(item_id, quote_plus(worksheet_id))
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def excel_add_chart(self, item_id, worksheet_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/charts/add".format(item_id, quote_plus(worksheet_id))
-        return self._post(url, **kwargs)
-
-    @token_required
-    def excel_get_tables(self, item_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/tables".format(item_id)
-        return self._get(url, params=params, **kwargs)
-
-    @token_required
-    def excel_add_table(self, item_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/tables/add".format(item_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def excel_add_column(self, item_id, worksheets_id, table_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/tables/{2}/columns".format(item_id, quote_plus(worksheets_id), table_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def excel_add_row(self, item_id, worksheets_id, table_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/tables/{2}/rows".format(item_id, quote_plus(worksheets_id), table_id)
-        return self._post(url, **kwargs)
-
-    @token_required
-    def excel_get_rows(self, item_id, table_id, params=None, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/tables/{1}/rows".format(item_id, table_id)
-        return self._get(url, params=params, **kwargs)
-
-    # @token_required
-    # def excel_get_cell(self, item_id, worksheets_id, params=None, **kwargs):
-    #     url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/Cell(row='1', column='A')".format(item_id, quote_plus(worksheets_id))
-    #     return self._get(url, params=params, **kwargs)
-
-    # @token_required
-    # def excel_add_cell(self, item_id, worksheets_id, **kwargs):
-    #     url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/rows".format(item_id, worksheets_id)
-    #     return self._patch(url, **kwargs)
-
-    @token_required
-    def excel_get_range(self, item_id, worksheets_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/range(address='A1:B2')".format(item_id, quote_plus(worksheets_id))
-        return self._get(url, **kwargs)
-
-    @token_required
-    def excel_update_range(self, item_id, worksheets_id, **kwargs):
-        url = "https://graph.microsoft.com/beta/me/drive/items/{0}/workbook/worksheets/{1}/range(address='A1:B2')".format(item_id, quote_plus(worksheets_id))
-        return self._patch(url, **kwargs)
-
+    #removed BETA calls
     def _get(self, url, **kwargs):
         return self._request('GET', url, **kwargs)
 
