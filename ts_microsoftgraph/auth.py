18fd36074f4965b30033f14790444f42798e8f6e
Joe Cincotta <joe@pixolut.com>
2020-09-07 07:52:14 +1000

    fix for multiple scope being passed in JSON

--- ts_microsoftgraph/auth.py
+++ ts_microsoftgraph/auth.py
@@ -183,6 +183,7 @@ class Auth(object):
             'grant_type': 'authorization_code',
             'scope': self._scope
         }
+        print(data)
         response = requests.post(self._authority + "/oauth2/v2.0/token", data=data)
         self._set_token(parse(response))
 
